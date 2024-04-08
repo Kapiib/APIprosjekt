e497d0fd157b83e1e37362ff910672b62eec3066
Kacper Baran <barankacper02@gmail.com>
2024-04-08 15:10:55 +0200

    push4

--- Del3/script.js
+++ Del3/script.js
@@ -15,11 +15,32 @@ function searchPokemon() {
 function displayPokemonInfo(pokemonData) {
     const pokemonInfoDiv = document.getElementById('pokemonInfo');
     const capitalizedPokemonName = pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1);
+
+    // Get sprite image URL from the "art" folder
+    const spriteUrl = `art/${pokemonData.id}.png`;
+
+    // Convert height from decimeters to feet and inches
+    const totalInches = pokemonData.height * 3.93701;
+    const feet = parseInt(totalInches / 12); // Extract feet without rounding
+    const inches = totalInches % 12; // Extract remaining inches
+
+    // Convert weight from hectograms to pounds
+    const weightInLbs = (pokemonData.weight) * 0.220462;
+
+    // Get type images from the "types" folder
+    const typeImages = pokemonData.types.map(type => `types/${type.type.name}.png`);
+
     pokemonInfoDiv.innerHTML = `
+        <h2>ID: ${pokemonData.id}</h2>
         <h2>Name: ${capitalizedPokemonName}</h2>
-        <p>Height: ${pokemonData.height}</p>
-        <p>Weight: ${pokemonData.weight}</p>
-        <p>Abilities:</p>
+        <img src="${spriteUrl}" alt="${capitalizedPokemonName}">
+        <p>Height: ${feet}' ${inches.toFixed()}"</p>
+        <p>Weight: ${weightInLbs.toFixed(2)} lbs</p>
+        <p>Type:</p>
+        <ul>
+            ${pokemonData.types.map((type, index) => `<li><img src="${typeImages[index]}" alt="${type.type.name}"></li>`).join('')}
+        </ul>
+        <p>Moves:</p>
         <ul>
             ${pokemonData.abilities.map(ability => `<li>${ability.ability.name}</li>`).join('')}
         </ul>
